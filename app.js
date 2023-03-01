389598cae175a20717ac72eb93d2bee2ed41f291
Carbon45 <thisisbusiness45@gmail.com>
2023-03-01 18:34:50 +0800

    most of my functions and bug fixes are done, just have the make the dot not repeat itself and im done with the functionality

--- app.js
+++ app.js
@@ -18,20 +18,20 @@
 
 // OPERATE
 
-function operate(operator, a, b) {
-    switch (operator) {
-      case '+':
-        return add(a, b);
-      case '-':
-        return subtract(a, b);
-      case '*':
-        return multiply(a, b);
-      case '/':
-        return divide(a, b);
-      default:
-        throw new Error(`Unknown operator '${operator}'`);
-    }
-  }
+// function operate(operator, a, b) {
+//     switch (operator) {
+//       case '+':
+//         return add(a, b);
+//       case '-':
+//         return subtract(a, b);
+//       case '*':
+//         return multiply(a, b);
+//       case '/':
+//         return divide(a, b);
+//       default:
+//         throw new Error(`Unknown operator '${operator}'`);
+//     }
+//   }
 
 
 // DISPLAYING THE NUMBERS
@@ -40,7 +40,7 @@ const numButtons = document.querySelectorAll('.num-button');
 const operators = document.querySelectorAll('.operator');
 
 const number = document.querySelector('.number');
-const secNum = document.querySelector('.sec-num');
+const secNumEl = document.querySelector('.sec-num');
 
 let isFirstNum = true;
 
@@ -54,6 +54,7 @@ function addNumToDisplay(button) {
   } else {
     secondNums += button.innerText;
     document.querySelector('.sec-num').innerText = secondNums;
+    console.log(secondNums)
   }
 }
 
@@ -69,26 +70,55 @@ function switchLoop() {
 
 operators.forEach((operator) => {
   operator.addEventListener('click', () => {
-    switchLoop();
+    if (isFirstNum === true) {
+      switchLoop();
 
-    // create the second number element
-    const secNumEl = document.createElement('h2');
-    secNumEl.classList.add('sec-num');
-    numDisplay.appendChild(secNumEl);
+      // create the second number element
+      const secNumEl = document.createElement('h2');
+      secNumEl.classList.add('sec-num');
+      numDisplay.appendChild(secNumEl);
+  
+      // clear the second number
+      // secondNums = '';
+      // numDisplay.querySelector('.sec-num').innerText = '';
+  
+      // display the operator
+      numDisplay.querySelector('.number').innerText += ` ${operator.innerText}`;
 
-    // clear the second number
-    // secondNums = '';
-    // numDisplay.querySelector('.sec-num').innerText = '';
+      console.log(storedNums);
+    }
+    else {
+      isFirstNum === false;
 
-    // display the operator
-    numDisplay.querySelector('.number').innerText += ` ${operator.innerText}`;
+      let fullNumbers = '';
+
+      fullNumbers += number.innerHTML + " " + secondNums;
+    
+      console.log(fullNumbers)  
+      const numResult = calculate(fullNumbers);
+    
+      number.textContent = `${numResult} ${operator.textContent}`
+      document.querySelector('.sec-num').remove();
+    
+      // resets the values
+      secondNums = '';
+      storedNums = numResult.toString();
+
+      // create second element
+      const secNumEl = document.createElement('h2');
+      secNumEl.classList.add('sec-num');
+      numDisplay.appendChild(secNumEl);
+    
+      console.log(isFirstNum);
+      // isFirstNum = true;    
+    }
+
   });
 });
 
 
 // CALCULATE 
 const equalsSign = document.querySelector('.equals-sign')
-const result = document.querySelector('.result');
 
 equalsSign.addEventListener('click', function() {
   let fullNumbers = '';
@@ -96,8 +126,18 @@ equalsSign.addEventListener('click', function() {
   fullNumbers += number.innerHTML + " " + secondNums;
 
   console.log(fullNumbers)  
+  const numResult = calculate(fullNumbers);
 
-  result.textContent = calculate(fullNumbers);
+  number.textContent = numResult
+  document.querySelector('.sec-num').remove();
+
+  // resets the values
+  secondNums = '';
+  storedNums = numResult.toString();
+
+  isFirstNum = true;
+
+  console.log(storedNums);
 })
 
 
@@ -109,8 +149,8 @@ function calculate(str) {
   // num2 = 10
   
   // turns the string numbers into real numbers
-  const n1 = parseInt(num1);
-  const n2 = parseInt(num2);
+  const n1 = parseFloat(num1);
+  const n2 = parseFloat(num2);
 
   // looks at the operator, calculates based on what operator you picked;
   switch (operator) {
@@ -127,3 +167,67 @@ function calculate(str) {
   }
 }
 
+// CLEAR BTN 
+
+const clearBtn = document.querySelector('.clear-btn');
+
+clearBtn.addEventListener('click', function() {
+  if (document.querySelector('.sec-num') !== null) {
+    number.innerHTML = '';
+    document.querySelector('.sec-num').remove();
+  }
+  else {
+    number.innerHTML = '';
+  }
+
+  fullNumbers = '';
+  secondNums = '';
+  storedNums = '';
+})
+
+// DELETE BTN
+
+const deleteBtn = document.querySelector('.delete-btn');
+
+deleteBtn.addEventListener('click', function() {
+  if (isFirstNum === true) {
+    storedNums.toString();
+
+    storedNums = storedNums.slice(0, -1);
+  
+    number.textContent = storedNums;
+  
+    console.log(storedNums);
+  }
+  else {
+    secondNums.toString();
+
+    secondNums = secondNums.slice(0, -1);
+
+    document.querySelector('.sec-num').textContent = secondNums;
+  }
+
+})
+
+// DOT 
+
+const dot = document.querySelector('.dot');
+
+dot.addEventListener('click', function() {
+  if (isFirstNum === true) {
+    storedNums.toString();
+
+    storedNums += '.';
+
+    number.textContent = storedNums;
+
+    console.log(storedNums);
+  }
+  else {
+    secondNums.toString();
+
+    secondNums += '.';
+
+    document.querySelector('.sec-num').textContent = secondNums;
+  }
+})
